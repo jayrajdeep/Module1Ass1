6e29660477c34f3b2aa8fc9e07565e2184717d34
Jaydeep <jayrajdeep123@gmail.com>
2020-05-29 09:25:55 +0530

    Assignment1

--- app.js
+++ app.js
@@ -5,10 +5,37 @@
 
   LunchCheckController.injector=['$scope','$filter',LunchCheckController]
   function LunchCheckController($scope,$filter){
-      $scope.lunchname="Enter Lunch Items";
+    //  $scope.lunchname="Enter Lunch Items";
+      $scope.clear = function()
+      {
+          $scope.Errmessage ="";
+          $scope.message ="";
+      }
 
-      $scope.check=function(){
-        return $scope.error="hello";
+      $scope.check = function() {
+        var a = $scope.lunchname;
+
+        if(a == null || a == "" || a == undefined){
+          $scope.message="";
+        return $scope.Errmessage="Please enter data first";
+        }
+        else
+        {
+          var items = a.split(',');
+          var arritems = items.filter(function(e){return e});
+        }
+
+        if(arritems.length <= 3){
+          console.log(arritems);
+          $scope.Errmessage="";
+        return $scope.message="Enjoy!";
+        }
+
+        if(arritems.length > 3){
+          console.log(arritems);
+          $scope.Errmessage="";
+        return $scope.message="Too much!";
+        }
       }
   }
 })();
